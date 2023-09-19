154046280b118c6f8fab5425c32fe92b4c0f9b0d
Mujahid Anwar <mujahidanwar141@gmail.com>
2023-09-20 00:05:14 +0500

    Footer Added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,32 @@
-import React from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import MovieCreationIcon from "@mui/icons-material/MovieCreation";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SearchIcon from "@mui/icons-material/Search";
+import "./Footer.css";
+import { Padding } from "@mui/icons-material";
+export default function SimpleBottomNavigation() {
+  const [value, setValue] = React.useState(0);
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
-
-export default Footer;
+  return (
+    <Box sx={{ width: 500 }}>
+      <div className="footer">
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction label="Trending" icon={<WhatshotIcon />} />
+          <BottomNavigationAction label="Movies" icon={<MovieCreationIcon />} />
+          <BottomNavigationAction label="Tv Series" icon={<LiveTvIcon />} />
+          <BottomNavigationAction label="Search" icon={<SearchIcon />} />
+        </BottomNavigation>
+      </div>
+    </Box>
+  );
+}
