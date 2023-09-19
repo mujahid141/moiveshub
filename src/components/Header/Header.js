39afd1512953f11ab47b5bc01fe0ff438d3b6724
Mujahid Anwar <mujahidanwar141@gmail.com>
2023-09-19 10:03:17 +0500

    header complete

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+import logo1 from "../../images/logo 1.jpg";
+import logo2 from "../../images/logo 2.png";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img className="logo1" src={logo1} alt="logo1" />
+      <h1>Moives Hub</h1>
+      <img className="logo2" src={logo2} alt="logo2" />
+    </div>
+  );
+};
+
+export default Header;
